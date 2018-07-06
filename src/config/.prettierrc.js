c8481a045c0a0d2845c2c409851a33c94710fc7e
Fredrik Palmquist <fredrik.palmquist@cybercom.com>
2018-07-06 14:59:54 +0200

    feat(prettier) expose config from root

---
+++ src/config/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+  jsxBracketSameLine: false,
+  singleQuote: true,
+};
--- src/scripts/format.js
+++ src/scripts/format.js
@@ -1,30 +1,30 @@
-const path = require("path");
-const spawn = require("cross-spawn");
-const { resolveBin } = require("../utils/utils");
+const path = require('path');
+const spawn = require('cross-spawn');
+const { resolveBin } = require('../utils/utils');
 
 const here = p => path.join(__dirname, p);
-const hereRelative = p => here(p).replace(process.cwd(), ".");
+const hereRelative = p => here(p).replace(process.cwd(), '.');
 
 const args = process.argv.slice(2);
 
-const config = ["--config", hereRelative("../config/.prettierrc")];
+const config = ['--config', hereRelative('../config/.prettierrc.js')];
 
-const ignore = ["--ignore-path", hereRelative("../config/.prettierignore")];
+const ignore = ['--ignore-path', hereRelative('../config/.prettierignore')];
 
-const write = args.includes("--no-write") ? [] : ["--write"];
+const write = args.includes('--no-write') ? [] : ['--write'];
 // this ensures that when running format as a pre-commit hook and we get
 // the full file path, we make that non-absolute so it is treated as a glob,
 // This way the prettierignore will be applied
-const relativeArgs = args.map(a => a.replace(`${process.cwd()}/`, ""));
+const relativeArgs = args.map(a => a.replace(`${process.cwd()}/`, ''));
 
 const filesToApply =
-  relativeArgs.length > 0 ? [] : ["**/*.+(js|json|less|css|ts|tsx|md)"];
+  relativeArgs.length > 0 ? [] : ['**/*.+(js|json|less|css|ts|tsx|md)'];
 
-const pathToModule = resolveBin("prettier");
+const pathToModule = resolveBin('prettier');
 const result = spawn.sync(
   pathToModule,
   [...config, ...ignore, ...write, ...filesToApply].concat(relativeArgs),
-  { stdio: "inherit" },
+  { stdio: 'inherit' },
 );
 
 process.exit(result.status);

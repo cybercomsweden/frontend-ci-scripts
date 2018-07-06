09b4c77fc3182cd4d9dc6c9bb7f9db5708ecf9ee
Fredrik Palmquist <fredrik.palmquist@cybercom.com>
2018-07-06 09:31:12 +0200

    fix(lint): fix linter warnings

--- src/scripts/lint.js
+++ src/scripts/lint.js
@@ -1,14 +1,12 @@
-"use strict";
-
-const CLIEngine = require("eslint").CLIEngine;
+const CLIEngine = require('eslint').CLIEngine;
 
 const cli = new CLIEngine({
   // @remove-on-eject-begin
-  configFile: require.resolve("eslint-config-react-app"),
+  configFile: require.resolve('eslint-config-react-app'),
   // @remove-on-eject-end
-  fix: process.argv.slice(2).indexOf("--fix") >= 0,
+  fix: process.argv.slice(2).indexOf('--fix') >= 0,
 });
-const report = cli.executeOnFiles(["src/**/*.{js,jsx,mjs}"]);
+const report = cli.executeOnFiles(['src/**/*.{js,jsx,mjs}']);
 const formatter = cli.getFormatter();
 
 // persist changed files when using --fix option

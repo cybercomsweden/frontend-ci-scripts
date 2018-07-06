511ea1fdc29950d878ceb75fe35aa74556456bbe
Fredrik Palmquist <fredrik.palmquist@cybercom.com>
2018-07-06 12:41:55 +0200

    chore(semver): update semver

--- src/index.js
+++ src/index.js
@@ -9,51 +9,51 @@
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on("unhandledRejection", err => {
+process.on('unhandledRejection', err => {
   throw err;
 });
 
-const chalk = require("chalk");
-const spawn = require("cross-spawn");
+const chalk = require('chalk');
+const spawn = require('cross-spawn');
 const args = process.argv.slice(2);
 
 const scriptIndex = args.findIndex(
   x =>
-    x === "lint" ||
-    x === "format" ||
-    x === "test" ||
-    x === "precommit" ||
-    x === "start",
+    x === 'lint' ||
+    x === 'format' ||
+    x === 'test' ||
+    x === 'precommit' ||
+    x === 'start',
 );
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
 
 switch (script) {
-  case "precommit":
-  case "lint":
-  case "format":
-  case "test":
-  case "start": {
-    console.log(chalk.cyan("Cybercom quality-scripts v0.5"));
+  case 'precommit':
+  case 'lint':
+  case 'format':
+  case 'test':
+  case 'start': {
+    console.log(chalk.cyan('Cybercom quality-scripts v0.6'));
     const result = spawn.sync(
-      "node",
+      'node',
       nodeArgs
-        .concat(require.resolve("./scripts/" + script))
+        .concat(require.resolve('./scripts/' + script))
         .concat(args.slice(scriptIndex + 1)),
-      { stdio: "inherit", env: getEnv() },
+      { stdio: 'inherit', env: getEnv() },
     );
     if (result.signal) {
-      if (result.signal === "SIGKILL") {
+      if (result.signal === 'SIGKILL') {
         console.log(
-          "The build failed because the process exited too early. " +
-            "This probably means the system ran out of memory or someone called " +
-            "`kill -9` on the process.",
+          'The build failed because the process exited too early. ' +
+            'This probably means the system ran out of memory or someone called ' +
+            '`kill -9` on the process.',
         );
-      } else if (result.signal === "SIGTERM") {
+      } else if (result.signal === 'SIGTERM') {
         console.log(
-          "The build failed because the process exited too early. " +
-            "Someone might have called `kill` or `killall`, or the system could " +
-            "be shutting down.",
+          'The build failed because the process exited too early. ' +
+            'Someone might have called `kill` or `killall`, or the system could ' +
+            'be shutting down.',
         );
       }
       process.exit(1);
@@ -63,7 +63,7 @@ switch (script) {
   }
   default:
     console.log('Unknown script "' + script + '".');
-    console.log("Perhaps you need to update the script package?");
+    console.log('Perhaps you need to update the script package?');
     break;
 }
 

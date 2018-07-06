a1cc0c90f280dbb65757a5b57a55f51ab4a56255
Linus Miller <lohfu@lohfu.io>
2018-07-06 12:20:11 +0200

    Use internal script directly instead of npm run format
    - If the format script in has globs, those globs seem to take
    precedence over the globs set in the lint-staged config

--- src/config/lintstagedrc.js
+++ src/config/lintstagedrc.js
@@ -5,7 +5,7 @@ const scriptPath = resolvePackagePath();
 module.exports = {
   concurrent: false,
   linters: {
-    "*.{js,jsx,json,less,css,ts,tsx,md}": [`npm run format`, "git add"],
+    "*.{js,jsx,json,less,css,ts,tsx,md}": [`${scriptPath} format`, "git add"],
     "*.{js,jsx,ts,tsx}": [
       `${scriptPath} lint`,
       `${scriptPath} test`,

261b93d8456c5870572b354433b1cad26ea45137
Fredrik Palmquist <fredrik.palmquist@cybercom.com>
2018-09-10 15:16:15 +0200

    fix(precommit): add npx for bin resolving in windows

--- src/config/lintstagedrc.js
+++ src/config/lintstagedrc.js
@@ -1,17 +1,17 @@
 const { resolvePackagePath } = require('../utils/utils');
 
-const scriptPath = resolvePackagePath();
-console.log(scriptPath);
+const scriptPath = resolvePackagePath().replace(/\\/g, '/');
+
 module.exports = {
   concurrent: false,
   linters: {
     '**/*.+(js|jsx|json|less|css|ts|tsx|md)': [
-      `${scriptPath} format`,
+      `npx ${scriptPath} format`,
       'git add',
     ],
     '**/*.+(js|jsx|ts|tsx)': [
-      `${scriptPath} lint`,
-      `${scriptPath} test --findRelatedTests --passWithNoTests`,
+      `npx ${scriptPath} lint`,
+      `npx ${scriptPath} test --findRelatedTests --passWithNoTests`,
       'git add',
     ],
   },
